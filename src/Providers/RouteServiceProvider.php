b5d8ee3ebaf51c6b3c55a8d5f2c8f7c4db44dd10
Steven Jimenez <sjimenez@riversagency.com>
2017-04-14 21:43:10 -0400

    fixes namespace on route service provider

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Infrastructure\Providers;
+namespace Orphan\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
